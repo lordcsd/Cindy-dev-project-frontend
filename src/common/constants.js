67a8e1b3b6d50149a4b6e2ca6a72af777676c8e3
lordcsd <dimgbachinonso@gmail.com>
2023-01-10 21:03:42 +0100

    added vendor name

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,4 +1,5 @@
 export const configConstants = {
     PLACES_KEY: 'AIzaSyD1pkxL93Aui_SU7h7u151CtWVMkufi28o',
+    // SERVER_ROOT_URL: 'http://localhost:4000'
     SERVER_ROOT_URL: 'https://xpress-meal-backend.cyclic.app'
 }
--- src/components/checkouts.js
+++ src/components/checkouts.js
@@ -6,10 +6,11 @@ let api, store;
 
 let EachCard = ({ props, baseURL, select }) => {
   let [showProducts, setShowProducts] = useState(false)
+  const { _id, paid, userName, products, date, delivered, vendor, address } = props;
 
   let markAsDelivered = () => {
     api
-      .patch(`${baseURL}/checkOut/setDelivered/${props._id}`)
+      .patch(`${baseURL}/checkOut/setDelivered/${_id}`)
       .then((res) => {
         alert(res.data.message);
       })
@@ -21,12 +22,12 @@ let EachCard = ({ props, baseURL, select }) => {
   console.log(props)
 
   return <div className="deliveryCard" onClick={select}>
-    <p>{props.userName}</p>
+    <p>{userName || ""}</p>
     <div className="deliveryCardProducts">
       <RemoveRedEyeOutlined className="icon" onClick={() => setShowProducts(!showProducts)} />
       {
         showProducts ?
-          props.products.map((each, index) =>
+          products.map((each, index) =>
             <div className="line" key={index}>
               <p className="productName">{each.productTitle}</p>
               <p>Price: {each.price}</p>
@@ -34,26 +35,27 @@ let EachCard = ({ props, baseURL, select }) => {
             </div>
           )
 
-          : (<p>{props.products.length} Products  </p>)
+          : (<p>{products.length} Products  </p>)
       }
 
     </div>
 
-    {props.date.includes("T") ? (
+    {date.includes("T") ? (
       <div>
-        <p>{props.date.split("T")[1].split(".")[0]}</p>
-        <p>{props.date.split("T")[0]}</p>
+        <p>{date.split("T")[1].split(".")[0]}</p>
+        <p>{date.split("T")[0]}</p>
       </div>
     ) : (
       <div></div>
     )}
 
-    {props["paid"] ? <p>{props.paid.total}</p> : <p></p>}
-    {props.address ? <p>{props.address.address}</p> : <p></p>}
+    {paid ? <p>{paid.total}</p> : <p></p>}
+    {address ? <p>{address.address}</p> : <p></p>}
     <div className="status">
-      <p>{props.delivered ? "Delivered" : "Not Delivered"}</p>
-      {props.delivered ? <CheckBox /> : <Close onClick={markAsDelivered} />}
+      <p>{delivered ? "Delivered" : "Not Delivered"}</p>
+      {delivered ? <CheckBox /> : <Close onClick={markAsDelivered} />}
     </div>
+    <p>{vendor && vendor.name}</p>
   </div>
 
 };
@@ -88,6 +90,7 @@ export default function Deliveries({ baseURL }) {
       <h4>Manage Deliveries</h4>
 
       <div className="deliveryCardsContain">
+
         <div className="deliveryCardsHeader">
           <p>UserName</p>
           <p>Products</p>
@@ -95,6 +98,7 @@ export default function Deliveries({ baseURL }) {
           <p>Paid</p>
           <p>Address</p>
           <p>Status</p>
+          <p>Vendor</p>
         </div>
         <div className="deliveryCards">
           {deliveries.map((each, index) =>
